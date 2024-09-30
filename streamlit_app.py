e973864fb4abcaec4023cecc1883cfb508b0dc92
mukusharma76 <mukeshjagatsharma@gmail.com>
2024-10-01 02:11:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,22 @@ import requests
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!."""
-)
-
+st.write("""Choose the fruits you want in your custom Smoothie!.""")
 name_on_order=st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be',name_on_order)
-
-
 #from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 session=cnx.session()
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+#convert the snowpark dataframe to a pandas dataframe so we can use loc function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 ingredients_list= st.multiselect('choose upto 5 ingredients',my_dataframe , max_selections=5 )
 if ingredients_list:
     ingredients_string =''
