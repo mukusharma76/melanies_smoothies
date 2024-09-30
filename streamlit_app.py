b89fea68eb3f0519d0f072d6c643776c97b4b4eb
mukusharma76 <mukeshjagatsharma@gmail.com>
2024-10-01 00:55:37 +0530

    Update streamlit_app.py
    typo in snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowlake.snowpark.functions import col
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
